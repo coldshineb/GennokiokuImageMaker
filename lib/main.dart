c9df5794105debc0472091b057cf3ec4eaa09565
coldshineb <shouhubyr@qq.com>
2024-03-18 13:36:19 -0400

    实现了站名显示

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,13 @@ import 'dart:io';
 import 'dart:typed_data';
 import 'dart:ui' as ui;
 import 'package:file_picker/file_picker.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 import 'package:main/station.dart';
-import 'package:path_provider/path_provider.dart';
+import 'dart:math';
 
 void main() {
   runApp(GennokiokuMetroLCDMaker());
@@ -25,14 +27,14 @@ class GennokiokuMetroLCDMaker extends StatelessWidget {
 
 class HomePage extends StatefulWidget {
   @override
-  _HomePageState createState() => _HomePageState();
+  HomePageState createState() => HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class HomePageState extends State<HomePage> {
   GlobalKey _globalKey = GlobalKey();
   File? _imageFile;
-  List<Text> _stationWidgets = [];
   List<Station> stations = [];
+  String jsonFileName = "";
 
   void _pickImage() async {
     FilePickerResult? pickedFile = await FilePicker.platform.pickFiles(
@@ -48,6 +50,7 @@ class _HomePageState extends State<HomePage> {
   }
 
   void _importStationName() async {
+    stations = [];
     // 用户选择 JSON 文件
     FilePickerResult? result = await FilePicker.platform.pickFiles(
       type: FileType.custom,
@@ -73,6 +76,7 @@ class _HomePageState extends State<HomePage> {
       } catch (e) {
         print('读取文件失败: $e');
       }
+      setState(() {});
     }
   }
 
@@ -86,29 +90,39 @@ class _HomePageState extends State<HomePage> {
             await image.toByteData(format: ui.ImageByteFormat.png);
         Uint8List pngBytes = byteData!.buffer.asUint8List();
 
-        final directory = await getApplicationDocumentsDirectory();
-        final String path = '${directory.path}/screenshot.png';
+        var saveFile = await FilePicker.platform.saveFile(
+            dialogTitle: "Select saving folder",
+            fileName: "运行中 + 站名.png",
+            type: FileType.image,
+            allowedExtensions: ["PNG"]);
+        final String path = '$saveFile';
         File imgFile = File(path);
-        await imgFile.writeAsBytes(pngBytes);
-
-        print('图片已保存至 $path');
-        showDialog(
-          context: context,
-          builder: (BuildContext context) {
-            return AlertDialog(
-              title: const Text('图片已导出'),
-              content: Text('图片已成功保存至: $path'),
-              actions: <Widget>[
-                TextButton(
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                  child: const Text('确定'),
-                ),
-              ],
-            );
-          },
-        );
+        if (path != "null") {
+          await imgFile.writeAsBytes(pngBytes);
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: const Text('图片已导出',
+                    style: TextStyle(fontFamily: "GennokiokuLCDFont")),
+                content: Text('图片已成功保存至: $path',
+                    style: TextStyle(fontFamily: "GennokiokuLCDFont")),
+                actions: <Widget>[
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text('确定'),
+                  ),
+                ],
+              );
+            },
+          );
+        } else {
+          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+            content: Text("取消导出"),
+          ));
+        }
       } catch (e) {
         print('导出图片失败: $e');
       }
@@ -118,85 +132,106 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('原忆轨道交通 LCD 生成器',
-              style: TextStyle(fontFamily: "FZLTHPro Global")),
-          foregroundColor: Colors.black,
-          backgroundColor: Colors.blue,
-          elevation: 5.0,
-        ),
-        body: Column(
-          children: [
-            Row(
+      appBar: AppBar(
+        title: const Text('原忆轨道交通 LCD 生成器',
+            style: TextStyle(fontFamily: "GennokiokuLCDFont")),
+        elevation: 4,
+      ),
+      body: ListView(
+        children: [
+          Row(
+            children: [
+              MenuBar(children: [
+                MenuItemButton(
+                  onPressed: _pickImage,
+                  child: const Text(
+                    "导入图片",
+                    style: TextStyle(
+                        fontFamily: "GennokiokuLCDFont", color: Colors.black),
+                  ),
+                ),
+                MenuItemButton(
+                  onPressed: _importStationName,
+                  child: const Text(
+                    "导入站名",
+                    style: TextStyle(
+                        fontFamily: "GennokiokuLCDFont", color: Colors.black),
+                  ),
+                ),
+                MenuItemButton(
+                  onPressed: _exportImage,
+                  child: const Text(
+                    "导出图片",
+                    style: TextStyle(
+                        fontFamily: "GennokiokuLCDFont", color: Colors.black),
+                  ),
+                )
+              ]),
+            ],
+          ),
+          Container(
+            color: Colors.red,
+            child: Stack(
               children: [
-                MenuBar(children: [
-                  MenuItemButton(
-                    onPressed: _pickImage,
-                    child: const Text(
-                      "导入图片",
-                      style: TextStyle(
-                          fontFamily: "FZLTHPro Global", color: Colors.black),
-                    ),
-                  ),
-                  MenuItemButton(
-                    onPressed: _importStationName,
-                    child: const Text(
-                      "导入站名",
-                      style: TextStyle(
-                          fontFamily: "FZLTHPro Global", color: Colors.black),
-                    ),
-                  ),
-                  MenuItemButton(
-                    onPressed: _exportImage,
-                    child: const Text(
-                      "导出图片",
-                      style: TextStyle(
-                          fontFamily: "FZLTHPro Global", color: Colors.black),
-                    ),
-                  )
-                ]),
+                _imageFile != null
+                    ? Image.file(_imageFile!, fit: BoxFit.fitWidth)
+                    : const Text(
+                        '导入图片',
+                        style: TextStyle(
+                          fontFamily: "GennokiokuLCDFont",
+                        ),
+                      ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(190, 165, 0, 0),
+                  child: showStationName(stations),
+                ),
               ],
             ),
-            RepaintBoundary(
-              key: _globalKey,
-              child: ImageContainer(
-                imageFile: _imageFile,
-              ),
-            ),
-          ],
-        ));
-  }
-}
-
-class StationNameContainer extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: [Text("123"), Text("456")],
+          )
+        ],
+      ),
     );
-    // TODO: implement build
-    throw UnimplementedError();
   }
-}
 
-class ImageContainer extends StatelessWidget {
-  final File? imageFile;
-
-  //final List<Station>? stationWidgets;
-
-  const ImageContainer({Key? key, required this.imageFile}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Stack(alignment: Alignment.topCenter, children: [
-      imageFile != null
-          ? Image.file(imageFile!, fit: BoxFit.fitWidth)
-          : const Text(
-              '选择右上角 导入图片',
-              style: TextStyle(
-                fontFamily: "FZLTHPro Global",
-              ),
+  Stack showStationName(List<Station> station) {
+    //TODO 文字大小随窗口改变适应
+    List<Container> tempList = [];
+    double count = 0;
+    for (var value in station) {
+      tempList.add(Container(
+        padding:
+            EdgeInsets.fromLTRB((1400 / (station.length - 1)) * count, 0, 0, 0),
+        child: Container(
+          transform: Matrix4.rotationZ(-0.75),
+          child: Text(
+            value.stationNameCN,
+            style: const TextStyle(
+              fontSize: 14,
+              fontFamily: "GennokiokuLCDFont",
+              color: Colors.black,
             ),
-    ]);
+          ),
+        ),
+      ));
+      tempList.add(Container(
+        padding:
+        EdgeInsets.fromLTRB(15+(1400 / (station.length - 1)) * count, 10, 0, 0),
+        child: Container(
+          transform: Matrix4.rotationZ(-0.75),
+          child: Text(
+            value.stationNameEN,
+            style: const TextStyle(
+              fontSize: 12,
+              fontFamily: "GennokiokuLCDFont",
+              color: Colors.black,
+            ),
+          ),
+        ),
+      ));
+      count++;
+    }
+    return Stack(
+      children: tempList,
+    );
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,14 +5,16 @@
 import FlutterMacOS
 import Foundation
 
-import file_saver
 import file_selector_macos
 import flutter_image_compress_macos
 import path_provider_foundation
+import screen_retriever
+import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FileSaverPlugin.register(with: registry.registrar(forPlugin: "FileSaverPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterImageCompressMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterImageCompressMacosPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  ScreenRetrieverPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverPlugin"))
+  WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
 }
