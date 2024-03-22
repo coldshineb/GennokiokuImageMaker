a728fc1a02599991df8eeceeb7100232e8862c36
coldshineb <shouhubyr@qq.com>
2024-03-22 17:05:47 -0400

    抽出线路名称标识以备将来复用

---
+++ lib/Util/Widgets.dart
@@ -0,0 +1,192 @@
+import 'package:flutter/material.dart';
+
+import '../Util.dart';
+
+class Widgets{
+  static Container lineNumber(Color lineColor, String lineNumber,String lineNumberEN) {
+    Container container = Container();
+
+    // 正则表达式
+    RegExp oneDigit = RegExp(r'^\d$'); // 匹配1个数字  1 2 3 4...
+    RegExp twoDigits = RegExp(r'^\d{2}$'); // 匹配2个数字  10 11 12 13...
+    RegExp oneDigitOneCharacter =
+    RegExp(r'^[a-zA-Z]\d$'); // 匹配1个字符1个数字  S1 S2 L1 L2 C5...
+    RegExp threeChineseCharacters = RegExp(r'^[\u4e00-\u9fff]{3}$'); // 匹配3个汉字
+    RegExp fourChineseCharacters =
+    RegExp(r'^[\u4e00-\u9fff]{4}$'); // 匹配4个汉字 南城环线  漓水环线 环山北线...
+    RegExp fiveChineseCharacters =
+    RegExp(r"^[\u4e00-\u9fff]{5}$"); // 匹配5个汉字 创新港环线...
+
+    //要达到完美显示效果，必须使用层叠组件，否则文字显示打架
+    //因此需要根据不同线路名称手动调节显示效果，不可使用动态调节
+    if (oneDigit.hasMatch(lineNumber)) {
+      container = Container(
+          width: 75,
+          height: 45,
+          decoration: BoxDecoration(
+            color: lineColor,
+            borderRadius: BorderRadius.circular(7.0),
+          ),
+          child: Stack(children: [
+            Positioned(
+              bottom: -2,
+              left: 5,
+              child: Text(
+                lineNumber,
+                style: TextStyle(
+                    fontSize: 40,
+                    fontFamily: "GennokiokuLCDFont",
+                    color: Util.getTextColorForBackground(lineColor)),
+              ),
+            ),
+            Positioned(
+              left: 29,
+              child: Text(
+                "号线",
+                style: TextStyle(
+                    fontSize: 18,
+                    fontFamily: "GennokiokuLCDFont",
+                    color: lineColor == Colors.transparent
+                        ? Colors.transparent
+                        : Util.getTextColorForBackground(lineColor)),
+              ),
+            ),
+            Positioned(
+              top: 22,
+              left: 28,
+              child: Text(
+                "Line $lineNumberEN",
+                style: TextStyle(
+                    fontSize: 14,
+                    fontFamily: "GennokiokuLCDFont",
+                    color: lineColor == Colors.transparent
+                        ? Colors.transparent
+                        : Util.getTextColorForBackground(lineColor)),
+              ),
+            ),
+          ]));
+    } else if (twoDigits.hasMatch(lineNumber) ||
+        oneDigitOneCharacter.hasMatch(lineNumber)) {
+      container = Container(
+          width: 105,
+          height: 45,
+          decoration: BoxDecoration(
+            color: lineColor,
+            borderRadius: BorderRadius.circular(7.0),
+          ),
+          child: Stack(children: [
+            Positioned(
+              bottom: -2,
+              left: 5,
+              child: Text(
+                lineNumber,
+                style: TextStyle(
+                    letterSpacing: -3,
+                    fontSize: 40,
+                    fontFamily: "GennokiokuLCDFont",
+                    color: Util.getTextColorForBackground(lineColor)),
+              ),
+            ),
+            Positioned(
+              left: 51,
+              child: Text(
+                "号线",
+                style: TextStyle(
+                    letterSpacing: 3,
+                    fontSize: 18,
+                    fontFamily: "GennokiokuLCDFont",
+                    color: lineColor == Colors.transparent
+                        ? Colors.transparent
+                        : Util.getTextColorForBackground(lineColor)),
+              ),
+            ),
+            Positioned(
+              top: 22,
+              left: 50,
+              child: Text(
+                "Line $lineNumberEN",
+                style: TextStyle(
+                    fontSize: 14,
+                    fontFamily: "GennokiokuLCDFont",
+                    color: lineColor == Colors.transparent
+                        ? Colors.transparent
+                        : Util.getTextColorForBackground(lineColor)),
+              ),
+            ),
+          ]));
+    } else if (fourChineseCharacters.hasMatch(lineNumber)) {
+      container = Container(
+          width: 95,
+          height: 45,
+          decoration: BoxDecoration(
+            color: lineColor,
+            borderRadius: BorderRadius.circular(7.0),
+          ),
+          child: Stack(children: [
+            Positioned(
+              left: 7,
+              child: Text(
+                lineNumber,
+                style: TextStyle(
+                    letterSpacing: 3,
+                    fontSize: 18,
+                    fontFamily: "GennokiokuLCDFont",
+                    color: lineColor == Colors.transparent
+                        ? Colors.transparent
+                        : Util.getTextColorForBackground(lineColor)),
+              ),
+            ),
+            Positioned(
+              top: 22,
+              left: 24,
+              child: Text(
+                "Line $lineNumberEN",
+                style: TextStyle(
+                    fontSize: 14,
+                    fontFamily: "GennokiokuLCDFont",
+                    color: lineColor == Colors.transparent
+                        ? Colors.transparent
+                        : Util.getTextColorForBackground(lineColor)),
+              ),
+            ),
+          ]));
+    } else if (fiveChineseCharacters.hasMatch(lineNumber)) {
+      container = Container(
+          width: 95,
+          height: 45,
+          decoration: BoxDecoration(
+            color: lineColor,
+            borderRadius: BorderRadius.circular(7.0),
+          ),
+          child: Stack(children: [
+            Positioned(
+              left: 7,
+              child: Text(
+                lineNumber,
+                style: TextStyle(
+                    letterSpacing: -1.9,
+                    fontSize: 18,
+                    fontFamily: "GennokiokuLCDFont",
+                    color: lineColor == Colors.transparent
+                        ? Colors.transparent
+                        : Util.getTextColorForBackground(lineColor)),
+              ),
+            ),
+            Positioned(
+              top: 22,
+              left: 24,
+              child: Text(
+                "Line $lineNumberEN",
+                style: TextStyle(
+                    fontSize: 14,
+                    fontFamily: "GennokiokuLCDFont",
+                    color: lineColor == Colors.transparent
+                        ? Colors.transparent
+                        : Util.getTextColorForBackground(lineColor)),
+              ),
+            ),
+          ]));
+    }
+    return container;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ import 'Util.dart';
 import 'Util/CustomColors.dart';
 import 'Util/CustomPainter.dart';
 import 'Util/CustomScrollBehavior.dart';
+import 'Util/Widgets.dart';
 
 void main() {
   runApp(const GennokiokuMetroLCDMaker());
@@ -265,7 +266,7 @@ class HomePageState extends State<HomePage> {
                               Container(
                                 padding:
                                     const EdgeInsets.fromLTRB(270, 16, 0, 0),
-                                child: showLineNumber(lineColor),
+                                child: Widgets.lineNumber(lineColor,lineNumber,lineNumberEN),
                               ),
                               Container(
                                   padding:
@@ -433,193 +434,6 @@ class HomePageState extends State<HomePage> {
         child: const Icon(Icons.refresh),
       ),
     );
-  }
-
-  Container showLineNumber(Color lineColor) {
-    Container container = Container();
-
-    // 正则表达式
-    RegExp oneDigit = RegExp(r'^\d$'); // 匹配1个数字  1 2 3 4...
-    RegExp twoDigits = RegExp(r'^\d{2}$'); // 匹配2个数字  10 11 12 13...
-    RegExp oneDigitOneCharacter =
-        RegExp(r'^[a-zA-Z]\d$'); // 匹配1个字符1个数字  S1 S2 L1 L2 C5...
-    RegExp threeChineseCharacters = RegExp(r'^[\u4e00-\u9fff]{3}$'); // 匹配3个汉字
-    RegExp fourChineseCharacters =
-        RegExp(r'^[\u4e00-\u9fff]{4}$'); // 匹配4个汉字 南城环线  漓水环线 环山北线...
-    RegExp fiveChineseCharacters =
-        RegExp(r"^[\u4e00-\u9fff]{5}$"); // 匹配5个汉字 创新港环线...
-
-    //要达到完美显示效果，必须使用层叠组件，否则文字显示打架
-    //因此需要根据不同线路名称手动调节显示效果，不可使用动态调节
-    if (oneDigit.hasMatch(lineNumber)) {
-      container = Container(
-          width: 75,
-          height: 45,
-          decoration: BoxDecoration(
-            color: lineColor,
-            borderRadius: BorderRadius.circular(7.0),
-          ),
-          child: Stack(children: [
-            Positioned(
-              bottom: -2,
-              left: 5,
-              child: Text(
-                lineNumber,
-                style: TextStyle(
-                    fontSize: 40,
-                    fontFamily: "GennokiokuLCDFont",
-                    color: Util.getTextColorForBackground(lineColor)),
-              ),
-            ),
-            Positioned(
-              left: 29,
-              child: Text(
-                "号线",
-                style: TextStyle(
-                    fontSize: 18,
-                    fontFamily: "GennokiokuLCDFont",
-                    color: lineColor == Colors.transparent
-                        ? Colors.transparent
-                        : Util.getTextColorForBackground(lineColor)),
-              ),
-            ),
-            Positioned(
-              top: 22,
-              left: 28,
-              child: Text(
-                "Line $lineNumberEN",
-                style: TextStyle(
-                    fontSize: 14,
-                    fontFamily: "GennokiokuLCDFont",
-                    color: lineColor == Colors.transparent
-                        ? Colors.transparent
-                        : Util.getTextColorForBackground(lineColor)),
-              ),
-            ),
-          ]));
-    } else if (twoDigits.hasMatch(lineNumber) ||
-        oneDigitOneCharacter.hasMatch(lineNumber)) {
-      container = Container(
-          width: 105,
-          height: 45,
-          decoration: BoxDecoration(
-            color: lineColor,
-            borderRadius: BorderRadius.circular(7.0),
-          ),
-          child: Stack(children: [
-            Positioned(
-              bottom: -2,
-              left: 5,
-              child: Text(
-                lineNumber,
-                style: TextStyle(
-                    letterSpacing: -3,
-                    fontSize: 40,
-                    fontFamily: "GennokiokuLCDFont",
-                    color: Util.getTextColorForBackground(lineColor)),
-              ),
-            ),
-            Positioned(
-              left: 51,
-              child: Text(
-                "号线",
-                style: TextStyle(
-                    letterSpacing: 3,
-                    fontSize: 18,
-                    fontFamily: "GennokiokuLCDFont",
-                    color: lineColor == Colors.transparent
-                        ? Colors.transparent
-                        : Util.getTextColorForBackground(lineColor)),
-              ),
-            ),
-            Positioned(
-              top: 22,
-              left: 50,
-              child: Text(
-                "Line $lineNumberEN",
-                style: TextStyle(
-                    fontSize: 14,
-                    fontFamily: "GennokiokuLCDFont",
-                    color: lineColor == Colors.transparent
-                        ? Colors.transparent
-                        : Util.getTextColorForBackground(lineColor)),
-              ),
-            ),
-          ]));
-    } else if (fourChineseCharacters.hasMatch(lineNumber)) {
-      container = Container(
-          width: 95,
-          height: 45,
-          decoration: BoxDecoration(
-            color: lineColor,
-            borderRadius: BorderRadius.circular(7.0),
-          ),
-          child: Stack(children: [
-            Positioned(
-              left: 7,
-              child: Text(
-                lineNumber,
-                style: TextStyle(
-                    letterSpacing: 3,
-                    fontSize: 18,
-                    fontFamily: "GennokiokuLCDFont",
-                    color: lineColor == Colors.transparent
-                        ? Colors.transparent
-                        : Util.getTextColorForBackground(lineColor)),
-              ),
-            ),
-            Positioned(
-              top: 22,
-              left: 24,
-              child: Text(
-                "Line $lineNumberEN",
-                style: TextStyle(
-                    fontSize: 14,
-                    fontFamily: "GennokiokuLCDFont",
-                    color: lineColor == Colors.transparent
-                        ? Colors.transparent
-                        : Util.getTextColorForBackground(lineColor)),
-              ),
-            ),
-          ]));
-    } else if (fiveChineseCharacters.hasMatch(lineNumber)) {
-      container = Container(
-          width: 95,
-          height: 45,
-          decoration: BoxDecoration(
-            color: lineColor,
-            borderRadius: BorderRadius.circular(7.0),
-          ),
-          child: Stack(children: [
-            Positioned(
-              left: 7,
-              child: Text(
-                lineNumber,
-                style: TextStyle(
-                    letterSpacing: -1.9,
-                    fontSize: 18,
-                    fontFamily: "GennokiokuLCDFont",
-                    color: lineColor == Colors.transparent
-                        ? Colors.transparent
-                        : Util.getTextColorForBackground(lineColor)),
-              ),
-            ),
-            Positioned(
-              top: 22,
-              left: 24,
-              child: Text(
-                "Line $lineNumberEN",
-                style: TextStyle(
-                    fontSize: 14,
-                    fontFamily: "GennokiokuLCDFont",
-                    color: lineColor == Colors.transparent
-                        ? Colors.transparent
-                        : Util.getTextColorForBackground(lineColor)),
-              ),
-            ),
-          ]));
-    }
-    return container;
   }
 
   //显示下一站/终点站下拉菜单内容
